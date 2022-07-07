3428f47a186d3004af8663b65fda78afbb46f11f
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-07 14:43:25 -0700

    Update websocket_manager.h

--- source/websocket_manager/websocket_manager.h
+++ source/websocket_manager/websocket_manager.h
@@ -2,15 +2,26 @@
 #define WEBSOCKET_MANAGER_H
 
 #include <QObject>
-
+#include <QList>
+#include <QtWebSockets/QtWebSockets>
+#include <QWebSocketServer>
 class websocket_manager : public QObject
 {
     Q_OBJECT
-public:
+	public:
     explicit websocket_manager(QObject *parent = nullptr);
-
-signals:
-
+    void initialize();
+		
+	private:
+		QWebSocketServer * ws_server;
+		
+		QList<QWebSocket *> clients;
+		QWebSocket * client;
+		
+	signals:
+		void new_connection();
+		
+		void new_text_message();
 };
 
 #endif // WEBSOCKET_MANAGER_H

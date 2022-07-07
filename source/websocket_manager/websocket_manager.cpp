41274d0e3080468472ae9448d86c4f391668cfcf
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-07-07 14:44:52 -0700

    Update websocket_manager.cpp
    All there is, is fighting, there is no head/brain/master, these problems will always exist as long as everything isnvirtualized on the hardware besides arithmetic and using numbers to pseudo representations

--- source/websocket_manager/websocket_manager.cpp
+++ source/websocket_manager/websocket_manager.cpp
@@ -1,6 +1,31 @@
-#include "websocket_manager.h"
-
 websocket_manager::websocket_manager(QObject *parent) : QObject(parent)
 {
 
+}
+
+void websocket_manager::initialize()
+{
+	ws_server = new QWebSocketServer("", QWebSocketServer::NonSecureMode, nullptr);
+
+	bool listen_success_status = ws_server->listen(QHostAddress::Any, 8080);
+	if(listen_success_status == true)
+	{
+		connect(ws_server, SIGNAL(newConnection()), this, SLOT(new_connection()));
+		
+	}
+	
+}
+
+void websocket_manager::new_connection()
+{
+	qDebug() << "new connection";
+	QWebSocket * client = ws_server->nextPendingConnection();
+	connect(client, SIGNAL(textMessageReceived()), this, SLOT(new_text_message()));
+	
+	clients << client;
+}
+
+void websocket_manager::new_text_message()
+{
+	
 }
